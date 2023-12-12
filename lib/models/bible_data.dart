6b58c703b5e8a3f87b760dda55c1d75d82a65ae6
sayantan200 <80565055+sayantan200@users.noreply.github.com>
2023-12-12 14:38:33 +0530

    Update bible_data.dart
    audio purpose

--- lib/models/bible_data.dart
+++ lib/models/bible_data.dart
@@ -2,10 +2,12 @@ class BibleData {
   final List<String> booksOfBibleTur;
   final List<String> booksOfBibleEng;
   final List<int> chaptersForAll;
+  final List<List<String>> turAudioName;
 
   BibleData({
     required this.booksOfBibleTur,
     required this.booksOfBibleEng,
     required this.chaptersForAll,
+    required this.turAudioName,
   });
 }
