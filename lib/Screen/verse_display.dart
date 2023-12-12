85725ed51ab6e9d0ee3e006a62eb8370f32f31b1
sayantan200 <80565055+sayantan200@users.noreply.github.com>
2023-12-13 04:21:57 +0530

    Update verse_display.dart
    audio slider

--- lib/Screen/verse_display.dart
+++ lib/Screen/verse_display.dart
@@ -2,8 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:audioplayers/audioplayers.dart';
 import 'package:bible/models/bible_data.dart';
+import 'package:bible/Services/bible_data_loader.dart';
 
-class VerseDisplayWidget extends StatelessWidget {
+class VerseDisplayWidget extends StatefulWidget {
   final String book;
   final int chapter;
   final String content;
@@ -19,10 +20,109 @@ class VerseDisplayWidget extends StatelessWidget {
   });
 
   @override
+  _VerseDisplayWidgetState createState() => _VerseDisplayWidgetState();
+}
+
+class _VerseDisplayWidgetState extends State<VerseDisplayWidget> {
+  AudioPlayer audioPlayer = AudioPlayer();
+  bool isPlaying = false;
+  int timeProgress = 0;
+  int audioDuration = 0;
+
+  Widget slider() {
+    return Column(
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(getTimeString(timeProgress)),
+            Text(getTimeString(audioDuration)),
+          ],
+        ),
+        Slider.adaptive(
+          value: timeProgress.toDouble(),
+          max: audioDuration.toDouble(),
+          onChanged: (value) {
+            seekToSec(value.toInt());
+          },
+        ),
+      ],
+    );
+  }
+
+  void seekToSec(int sec) {
+    Duration newPos = Duration(seconds: sec);
+    audioPlayer.seek(newPos);
+  }
+
+  String getTimeString(int seconds) {
+    String minuteString = '${(seconds ~/ 60).toString().padLeft(2, '0')}';
+    String secondString = '${(seconds % 60).toString().padLeft(2, '0')}';
+    return '$minuteString:$secondString';
+  }
+
+  pauseMusic() async {
+    await audioPlayer.pause();
+    setState(() {
+      isPlaying = false;
+    });
+  }
+
+  playMusic(String audioUrl) async {
+    await audioPlayer.play(UrlSource(audioUrl));
+    setState(() {
+      isPlaying = true;
+    });
+  }
+
+  late BibleData bibleData;
+
+  Future<void> _loadBibleData() async {
+    try {
+      bibleData = await BibleDataService().loadBibleData();
+    } catch (e) {
+      print('Error loading Bible data: $e');
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text('Error loading Bible data. Please try again.'),
+        ),
+      );
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    audioPlayer = AudioPlayer();
+
+    audioPlayer.onDurationChanged.listen((Duration duration) {
+      setState(() {
+        audioDuration = duration.inSeconds;
+      });
+    });
+
+    
+    
+    /*audioPlayer.onPlayerUpdate.listen((event) {
+      setState(() {
+        timeProgress = event.position.inSeconds;
+      });
+    });*/
+
+    _loadBibleData();
+  }
+
+  @override
+  void dispose() {
+    audioPlayer.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Verses - $book Chapter $chapter'),
+        title: Text('Verses - ${widget.book} Chapter ${widget.chapter}'),
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
@@ -31,35 +131,44 @@ class VerseDisplayWidget extends StatelessWidget {
             Expanded(
               child: SingleChildScrollView(
                 child: Text(
-                  content,
+                  widget.content,
                   style: const TextStyle(fontSize: 23.0),
                 ),
               ),
             ),
             IconButton(
-              icon: Icon(Icons.play_arrow),
-              onPressed: () {
-                // Check if the book is in the New Testament and the language is Turkish
-                if (book.indexOf(book) >= 39 && selectedLanguage == 'Turkish') {
-                  String audioName = BibleData
-                      .turAudioName[book.indexOf(book) - 39][chapter - 1];
-                  String audioUrl =
-                      'https://incil.online/data/files/$audioName.mp3';
-
-                  AudioPlayer audioPlayer = AudioPlayer();
-                  audioPlayer.play(UrlSource(audioUrl));
+              icon: Icon(isPlaying ? Icons.pause : Icons.play_arrow),
+              onPressed: () async {
+                if (isPlaying) {
+                  pauseMusic();
                 } else {
-                  // Display a message or take appropriate action if conditions are not met
-                  // For example, you can show a snackbar or toast message
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    SnackBar(
-                      content: Text(
-                          'Audio not available for this book or language.'),
-                    ),
-                  );
+                  // Check if the book is in the New Testament and the language is Turkish
+                  if (widget.book.indexOf(widget.book) >= 0 &&
+                      widget.selectedLanguage == 'Turkish') {
+                    String audioName =
+                        bibleData.turAudioName[widget.book.indexOf(widget.book)]
+                            [widget.chapter - 1];
+                    String audioUrl =
+                        'https://incil.online/data/files/$audioName.mp3';
+
+                    playMusic(audioUrl);
+                  } else {
+                    // Display a message or take appropriate action if conditions are not met
+                    // For example, you can show a snackbar or toast message
+                    print(
+                        'The selected language is: ${widget.selectedLanguage} ${widget.book.indexOf(widget.book)}');
+
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(
+                        content: Text(
+                            'Audio not available for this book or language.'),
+                      ),
+                    );
+                  }
                 }
               },
             ),
+            if (isPlaying) slider(),
             const SizedBox(height: 16.0),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceAround,
@@ -82,14 +191,15 @@ class VerseDisplayWidget extends StatelessWidget {
 
   // Navigate to the next chapter content
   void _navigateToNextChapter(BuildContext context) async {
-    int nextChapter = chapter + 1;
+    int nextChapter = widget.chapter + 1;
 
-    if (nextChapter > maxChapters.last) {
+    if (nextChapter > widget.maxChapters.last) {
       // If the next chapter exceeds the last chapter of the current book, check for the next book
-      int nextBookIndex = maxChapters.indexOf(maxChapters.firstWhere((element) => element > chapter));
+      int nextBookIndex = widget.maxChapters.indexOf(
+          widget.maxChapters.firstWhere((element) => element > widget.chapter));
 
-      if (nextBookIndex >= 0 && nextBookIndex < maxChapters.length) {
-        String nextBook = maxChapters[nextBookIndex].toString();
+      if (nextBookIndex >= 0 && nextBookIndex < widget.maxChapters.length) {
+        String nextBook = widget.maxChapters[nextBookIndex].toString();
         nextChapter = 1; // Reset to the first chapter of the next book
 
         _navigateToChapterContent(context, nextBook, nextChapter);
@@ -98,13 +208,15 @@ class VerseDisplayWidget extends StatelessWidget {
       }
     } else {
       // Otherwise, navigate to the next chapter in the current book
-      _navigateToChapterContent(context, book, nextChapter);
+      _navigateToChapterContent(context, widget.book, nextChapter);
     }
   }
 
   // Navigate to a specific chapter content
-  void _navigateToChapterContent(BuildContext context, String book, int chapter) async {
-    String filePath = 'assets/${selectedLanguage == 'English' ? 'Eng' : 'Tur'}/$book/$book$chapter.txt';
+  void _navigateToChapterContent(
+      BuildContext context, String book, int chapter) async {
+    String filePath =
+        'assets/${widget.selectedLanguage == 'English' ? 'Eng' : 'Tur'}/$book/$book$chapter.txt';
     String chapterContent = await rootBundle.loadString(filePath);
 
     Navigator.pushReplacement(
@@ -114,8 +226,8 @@ class VerseDisplayWidget extends StatelessWidget {
           book: book,
           chapter: chapter,
           content: chapterContent,
-          maxChapters: maxChapters,
-          selectedLanguage: selectedLanguage,
+          maxChapters: widget.maxChapters,
+          selectedLanguage: widget.selectedLanguage,
         ),
       ),
     );
@@ -123,23 +235,26 @@ class VerseDisplayWidget extends StatelessWidget {
 
   // Navigate to the previous chapter content
   void _navigateToPreviousChapter(BuildContext context) async {
-    int previousChapter = chapter - 1;
+    int previousChapter = widget.chapter - 1;
 
     if (previousChapter < 1) {
       // If the previous chapter is less than 1, check for the previous book
-      int previousBookIndex = maxChapters.indexOf(maxChapters.firstWhere((element) => element >= chapter)) - 1;
+      int previousBookIndex = widget.maxChapters.indexOf(widget.maxChapters
+              .firstWhere((element) => element >= widget.chapter)) -
+          1;
 
       if (previousBookIndex >= 0) {
-        String previousBook = maxChapters[previousBookIndex].toString();
-        int lastChapterOfPreviousBook = maxChapters[previousBookIndex];
+        String previousBook = widget.maxChapters[previousBookIndex].toString();
+        int lastChapterOfPreviousBook = widget.maxChapters[previousBookIndex];
 
-        _navigateToChapterContent(context, previousBook, lastChapterOfPreviousBook);
+        _navigateToChapterContent(
+            context, previousBook, lastChapterOfPreviousBook);
       } else {
         print('Already at the beginning of the Bible');
       }
     } else {
       // Otherwise, navigate to the previous chapter in the current book
-      _navigateToChapterContent(context, book, previousChapter);
+      _navigateToChapterContent(context, widget.book, previousChapter);
     }
   }
 }
