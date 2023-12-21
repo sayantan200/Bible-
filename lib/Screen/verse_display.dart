85e3a35dd794f7bed775bde0c4733d63dfd562ab
sayantan200 <80565055+sayantan200@users.noreply.github.com>
2023-12-21 22:36:37 +0530

    Update verse_display.dart

--- lib/Screen/verse_display.dart
+++ lib/Screen/verse_display.dart
@@ -26,6 +26,7 @@ class VerseDisplayWidget extends StatefulWidget {
 
 class _VerseDisplayWidgetState extends State<VerseDisplayWidget> {
   AudioPlayer audioPlayer = AudioPlayer();
+  bool shouldAutoPlay = true;
   bool isPlaying = false;
   int timeProgress = 0;
   int audioDuration = 0;
@@ -69,18 +70,38 @@ class _VerseDisplayWidgetState extends State<VerseDisplayWidget> {
     });
   }
 
-  playMusic(String audioUrl) async {
-    await audioPlayer.play(UrlSource(audioUrl));
-    setState(() {
-      isPlaying = true;
-    });
+  void playMusic(String audioUrl) async {
+    try {
+      await audioPlayer.play(UrlSource(audioUrl));
+      if (mounted) {
+        setState(() {
+          isPlaying = true;
+        });
+      }
+    } catch (e) {
+      print('Error playing audio: $e');
+      // Handle the error, e.g., show a message to the user
+    }
   }
 
   late BibleData bibleData;
 
   Future<void> _loadBibleData() async {
     try {
+      if (!mounted) return; // Check if the widget is still mounted
+
+      setState(() {
+        // Set up a loading state if needed
+      });
+
       bibleData = await BibleDataService().loadBibleData();
+
+      if (mounted) {
+        // Check again if the widget is still mounted before setting the state
+        setState(() {
+          // Clear the loading state if needed
+        });
+      }
     } catch (e) {
       print('Error loading Bible data: $e');
       ScaffoldMessenger.of(context).showSnackBar(
@@ -96,6 +117,8 @@ class _VerseDisplayWidgetState extends State<VerseDisplayWidget> {
     super.initState();
     audioPlayer = AudioPlayer();
 
+    _loadBibleData();
+
     audioPlayer.onDurationChanged.listen((Duration duration) {
       setState(() {
         audioDuration = duration.inSeconds;
@@ -110,7 +133,12 @@ class _VerseDisplayWidgetState extends State<VerseDisplayWidget> {
       });
     });
 
-    _loadBibleData();
+    audioPlayer.onPlayerComplete.listen((event) {
+      // Audio playback completed, navigate to the next chapter and start playing if needed
+      if (shouldAutoPlay) {
+        _navigateToNextChapter(context);
+      }
+    });
   }
 
   @override
@@ -121,9 +149,15 @@ class _VerseDisplayWidgetState extends State<VerseDisplayWidget> {
 
   @override
   Widget build(BuildContext context) {
+    if (bibleData == null) {
+      return CircularProgressIndicator(); // You can replace this with your loading widget
+    }
+
     return Scaffold(
       appBar: AppBar(
-        title: Text('Verses - ${widget.book} Chapter ${widget.chapter}'),
+        backgroundColor: Color.fromARGB(255, 52, 2, 230),
+        title: Text(' ${widget.book}     ${widget.chapter}'),
+        centerTitle: true,
         actions: [
           // Add the email icon button
           IconButton(
@@ -137,8 +171,16 @@ class _VerseDisplayWidgetState extends State<VerseDisplayWidget> {
           ),
         ],
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
+      body: GestureDetector(
+        onHorizontalDragEnd: (details) {
+          if (details.primaryVelocity! > 0) {
+            // Swipe from left to right (previous chapter)
+            _navigateToPreviousChapter(context);
+          } else {
+            // Swipe from right to left (next chapter)
+            _navigateToNextChapter(context);
+          }
+        },
         child: Column(
           children: [
             Expanded(
@@ -149,66 +191,88 @@ class _VerseDisplayWidgetState extends State<VerseDisplayWidget> {
                 ),
               ),
             ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                // Left button
-                ElevatedButton(
-                  onPressed: () => _navigateToPreviousChapter(context),
-                  child: const Text(
-                    '<',
-                    style:
-                        TextStyle(fontSize: 25.0, fontWeight: FontWeight.bold),
+            Container(
+              color: Color.fromARGB(255, 38, 83, 130),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  // Left button
+                  ElevatedButton(
+                    onPressed: () => _navigateToPreviousChapter(context),
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Color.fromARGB(255, 52, 2,
+                          230), // Set the background color to transparent
+                    ),
+                    child: const Text(
+                      '<',
+                      style: TextStyle(
+                          fontSize: 25.0, fontWeight: FontWeight.bold),
+                    ),
                   ),
-                ),
-                // Play button
-                IconButton(
-                  icon: Icon(isPlaying ? Icons.pause : Icons.play_arrow),
-                  onPressed: () async {
-                    if (isPlaying) {
-                      pauseMusic();
-                    } else {
-                      // Check if the book is in the New Testament and the language is Turkish
-                      int bookIndex =
-                          bibleData.booksOfBibleTur.indexOf(widget.book);
-                      if (bookIndex >= 39 &&
-                          widget.selectedLanguage == 'Turkish') {
-                        print('$bookIndex');
-                        String audioName = bibleData
-                            .turAudioName[bookIndex - 39][widget.chapter - 1];
-                        String audioUrl =
-                            'https://incil.online/data/files/$audioName.mp3';
+                  // Play button
+                  if (getBookIndex() >= 39 &&
+                      widget.selectedLanguage == 'Turkish')
+                    IconButton(
+                      icon: Image.asset(
+                        isPlaying
+                            ? 'assets/images/pausebutton.png'
+                            : 'assets/images/playbutton.png',
+                        width: 24.0, // Set the width as needed
+                        height: 24.0, // Set the height as needed
+                        // Set the height as needed
+                      ),
+                      onPressed: () async {
+                        if (isPlaying) {
+                          pauseMusic();
+                        } else {
+                          // Check if the book is in the New Testament and the language is Turkish
+                          int bookIndex =
+                              bibleData.booksOfBibleTur.indexOf(widget.book);
+                          if (bookIndex >= 39 &&
+                              widget.selectedLanguage == 'Turkish') {
+                            print('$bookIndex');
+                            String audioName =
+                                bibleData.turAudioName[bookIndex - 39]
+                                    [widget.chapter - 1];
+                            String audioUrl =
+                                'https://incil.online/data/files/$audioName.mp3';
 
-                        playMusic(audioUrl);
-                      } else {
-                        // Display a message or take appropriate action if conditions are not met
-                        // For example, you can show a snackbar or toast message
-                        print(
-                            'The selected language is: ${widget.selectedLanguage} ${widget.book.indexOf(widget.book)}');
+                            playMusic(audioUrl);
+                          } else {
+                            // Display a message or take appropriate action if conditions are not met
+                            // For example, you can show a snackbar or toast message
+                            print(
+                                'The selected language is: ${widget.selectedLanguage} ${widget.book.indexOf(widget.book)}');
 
-                        ScaffoldMessenger.of(context).showSnackBar(
-                          SnackBar(
-                            content: Text(
-                                'Audio not available for this book or language.'),
-                          ),
-                        );
-                      }
-                    }
-                  },
-                ),
-                // Right button
-                ElevatedButton(
-                  onPressed: () => _navigateToNextChapter(context),
-                  child: const Text(
-                    '>',
-                    style:
-                        TextStyle(fontSize: 25.0, fontWeight: FontWeight.bold),
+                            ScaffoldMessenger.of(context).showSnackBar(
+                              SnackBar(
+                                content: Text(
+                                    'Audio not available for this book or language.'),
+                              ),
+                            );
+                          }
+                        }
+                      },
+                    ),
+                  // Right button
+                  ElevatedButton(
+                    onPressed: () => _navigateToNextChapter(context),
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Color.fromARGB(255, 52, 2,
+                          230), // Set the background color to transparent
+                    ),
+                    child: const Text(
+                      '>',
+                      style: TextStyle(
+                        fontSize: 25.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
             if (isPlaying) slider(),
-            const SizedBox(height: 16.0),
           ],
         ),
       ),
@@ -219,13 +283,16 @@ class _VerseDisplayWidgetState extends State<VerseDisplayWidget> {
   void _navigateToNextChapter(BuildContext context) async {
     int nextChapter = widget.chapter + 1;
 
-    if (nextChapter > widget.maxChapters.last) {
+    if (nextChapter > bibleData.chaptersForAll[getBookIndex()]) {
       // If the next chapter exceeds the last chapter of the current book, check for the next book
-      int nextBookIndex = widget.maxChapters.indexOf(
-          widget.maxChapters.firstWhere((element) => element > widget.chapter));
+      int nextBookIndex = getBookIndex() + 1;
 
-      if (nextBookIndex >= 0 && nextBookIndex < widget.maxChapters.length) {
-        String nextBook = widget.maxChapters[nextBookIndex].toString();
+      if (nextBookIndex >= 0 &&
+          nextBookIndex < bibleData.chaptersForAll.length) {
+        String nextBook = widget.selectedLanguage == 'English'
+            ? bibleData.booksOfBibleEng[nextBookIndex]
+            : bibleData.booksOfBibleTur[nextBookIndex];
+        ;
         nextChapter = 1; // Reset to the first chapter of the next book
 
         _navigateToChapterContent(context, nextBook, nextChapter);
@@ -257,30 +324,62 @@ class _VerseDisplayWidgetState extends State<VerseDisplayWidget> {
         ),
       ),
     );
+    if (shouldAutoPlay) {
+      playMusicForChapter(book, chapter);
+    }
   }
 
   // Navigate to the previous chapter content
   void _navigateToPreviousChapter(BuildContext context) async {
     int previousChapter = widget.chapter - 1;
+    int previousBookIndex = getBookIndex();
+
+    ;
 
     if (previousChapter < 1) {
+      print("hello2");
       // If the previous chapter is less than 1, check for the previous book
-      int previousBookIndex = widget.maxChapters.indexOf(widget.maxChapters
-              .firstWhere((element) => element >= widget.chapter)) -
-          1;
+      previousBookIndex = getBookIndex() - 1;
+      print("entered{$previousBookIndex}");
 
       if (previousBookIndex >= 0) {
-        String previousBook = widget.maxChapters[previousBookIndex].toString();
-        int lastChapterOfPreviousBook = widget.maxChapters[previousBookIndex];
+        String previousBook = widget.selectedLanguage == 'English'
+            ? bibleData.booksOfBibleEng[previousBookIndex]
+            : bibleData.booksOfBibleTur[previousBookIndex];
+        int lastChapterOfPreviousBook =
+            bibleData.chaptersForAll[previousBookIndex];
+        print("hello 1 {$lastChapterOfPreviousBook}");
 
         _navigateToChapterContent(
             context, previousBook, lastChapterOfPreviousBook);
       } else {
-        print('Already at the beginning of the Bible');
+        print('Already at the beginning of the Bible ${getBookIndex()}');
       }
     } else {
+      print(
+          'Navigating to previous chapter. Current Book Index: ${getBookIndex()}  and ');
       // Otherwise, navigate to the previous chapter in the current book
       _navigateToChapterContent(context, widget.book, previousChapter);
     }
+  }
+
+  void playMusicForChapter(String book, int chapter) {
+    
+    int bookIndex = getBookIndex();
+    if (bookIndex >= 39 && widget.selectedLanguage == 'Turkish') {
+      String audioName = bibleData.turAudioName[bookIndex - 39][widget.chapter];
+      String audioUrl = 'https://incil.online/data/files/$audioName.mp3';
+      print("Entered in playMusicForChapter $audioUrl");
+      playMusic(audioUrl);
+    }
+  }
+
+  int getBookIndex() {
+    // Calculate the current book index
+    int bookIndex = widget.selectedLanguage == 'English'
+        ? bibleData.booksOfBibleEng.indexOf(widget.book)
+        : bibleData.booksOfBibleTur.indexOf(widget.book);
+
+    return bookIndex;
   }
 }
