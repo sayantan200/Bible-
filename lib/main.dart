5896f87b8be460997f6212f86194c5732d6368eb
sayantan200 <80565055+sayantan200@users.noreply.github.com>
2023-12-26 03:09:03 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,40 @@
 import 'package:flutter/material.dart';
 import 'package:bible/Screen/book_selection.dart';
+import 'package:bible/Screen/verse_display.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
       title: 'Bible App',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: BookSelectionWidget(),
+      home: FutureBuilder(
+        future: DefaultAssetBundle.of(context)
+            .loadString('assets/Tur/Matta/Matta1.txt'),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return VerseDisplayWidget(
+              book:
+                  'Matta', // You can use the actual value or retrieve it from your data
+              chapter: 1,
+              content: snapshot.data
+                  .toString(), // Pass the loaded chapter content here
+              maxChapters: [], // Provide max chapters if needed
+              selectedLanguage: 'Turkish',
+            );
+          } else {
+            return CircularProgressIndicator(); // Show a loading indicator while the content is loading
+          }
+        },
+      ),
     );
   }
 }
+
